223aec4917becfb1bdb040b523d6973cd8872b84
Jeremy Rubin <j@rubin.io>
2019-10-16 15:04:09 -0700

    Make Bare OP_CHECKTEMPLATEVERIFY basic transactions standard

--- src/wallet/scriptpubkeyman.cpp
+++ src/wallet/scriptpubkeyman.cpp
@@ -196,6 +196,11 @@ IsMineResult IsMineInner(const LegacyScriptPubKeyMan& keystore, const CScript& s
         }
         break;
     }
+    case TxoutType::TX_STANDARDTEMPLATE:
+    {
+        ret = IsMineResult::NO;
+        break;
+    }
     } // no default case, so the compiler can warn about missing cases
 
     if (ret == IsMineResult::NO && keystore.HaveWatchOnly(scriptPubKey)) {
